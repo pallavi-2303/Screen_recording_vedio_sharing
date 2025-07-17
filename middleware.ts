2b76250df064e47e3ee5b342868191a8f52efdb8
pallavi-2303 <singh.pallavikumari14@gmail.com>
2025-07-17 18:50:25 +0530

    middleware updated

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "./lib/auth";
 import { headers } from "next/headers";
-import aj from "./lib/arcjet";
-import { createMiddleware, detectBot, shield } from "@arcjet/next";
+
 
 export async function middleware(request:NextRequest,response:NextResponse){
     const session=await auth.api.getSession({
@@ -13,10 +12,6 @@ headers:await headers()
     }
     return NextResponse.next();
 }
-const validate=aj
-.withRule(shield({mode:'LIVE'}))
-.withRule(detectBot({mode:'LIVE',allow:['CATEGORY:SEARCH_ENGINE','GOOGLE_CRAWLER']}))
-export default createMiddleware(validate)
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sign-in|assets).*)"],
 };
